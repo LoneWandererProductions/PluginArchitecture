ef61131be717ef1079f0bc708fc0f856e7db6f6b
LoneWandererProductions <peter.geinitz@freenet.de>
2026-01-23 12:58:13 +0100

    stub out our menu more

---
+++ PluginLoader/EnumToBooleanConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace PluginLoader
+{
+    public class EnumToBooleanConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value?.ToString() == parameter?.ToString();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if ((bool)value)
+                return Enum.Parse(targetType, parameter.ToString()!);
+            return Binding.DoNothing;
+        }
+    }
+}
